eebafe8d072432776357b9dad8abbc5b0dc48068
keteng <yang.chen@fabric4cloud.com>
2018-01-12 09:43:40 +0800

    修改ycm_extra_conf 文件

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -32,9 +32,8 @@ BASE_FLAGS = [
         '-isystem',
         '/System/Library/Frameworks'
         '-isystem',
-        '/Library/Frameworks',
-        '-DNN_HAVE_KQUEUE=1',
-        ] + '-DNN_HAVE_BACKTRACE=1 -DNN_HAVE_GCC_ATOMIC_BUILTINS -DNN_HAVE_KQUEUE=1 -DNN_HAVE_MSG_CONTROL=1 -DNN_HAVE_OSX -DNN_HAVE_PIPE=1 -DNN_HAVE_POLL=1 -DNN_HAVE_SEMAPHORE -DNN_HAVE_SEMAPHORE_PTHREAD=1 -DNN_HAVE_SOCKETPAIR=1 -DNN_HAVE_UNIX_SOCKETS=1 -DNN_MAX_SOCKETS=512 -D_GNU_SOURCE -D_POSIX_PTHREAD_SEMANTICS -D_REENTRANT -D_THREAD_SAFE'.split(" ") + '-DNN_HAVE_BACKTRACE=1 -DNN_HAVE_GCC_ATOMIC_BUILTINS -DNN_HAVE_KQUEUE=1 -DNN_HAVE_MSG_CONTROL=1 -DNN_HAVE_OSX -DNN_HAVE_PIPE=1 -DNN_HAVE_POLL=1 -DNN_HAVE_SEMAPHORE -DNN_HAVE_SEMAPHORE_PTHREAD=1 -DNN_HAVE_SOCKETPAIR=1 -DNN_HAVE_UNIX_SOCKETS=1 -DNN_MAX_SOCKETS=512 -DNN_SHARED_LIB -DNN_USE_KQUEUE -DNN_USE_PIPE -D_GNU_SOURCE -D_POSIX_PTHREAD_SEMANTICS -D_REENTRANT -D_THREAD_SAFE -Dnanomsg_EXPORTS'.split(" ")
+        '/Library/Frameworks'
+        ]
 
 
 SOURCE_EXTENSIONS = [
@@ -170,8 +169,6 @@ def FlagsForCompilationDatabase(root, filename):
         # Last argument of next function is the name of the build folder for
         # out of source projects
         compilation_db_path = FindNearest(root, 'compile_commands.json', BUILD_DIRECTORY)
-        with open("/tmp/a.txt") as f:
-            f.write(compilation_db_path)
         compilation_db_dir = os.path.dirname(compilation_db_path)
         logging.info("Set compilation database directory to " + compilation_db_dir)
         compilation_db = ycm_core.CompilationDatabase(compilation_db_dir)
